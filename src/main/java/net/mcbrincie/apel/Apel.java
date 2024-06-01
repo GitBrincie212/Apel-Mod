956b498f14bf0c0025cd5c876e749f25778e06d4
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2024-06-01 13:53:53 +0300

    Added separate thread for executing draw tasks

--- src/main/java/net/mcbrincie/apel/Apel.java
+++ src/main/java/net/mcbrincie/apel/Apel.java
@@ -1,21 +1,19 @@
 package net.mcbrincie.apel;
 
 import net.fabricmc.api.ModInitializer;
-
 import net.mcbrincie.apel.item.ModItems;
-import net.mcbrincie.apel.lib.util.DelayedTask;
 import net.mcbrincie.apel.lib.util.scheduler.ApelScheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Apel implements ModInitializer {
 	public static final String mod_id = "apel";
     public static final Logger LOGGER = LoggerFactory.getLogger(mod_id);
 	public static ApelScheduler apelScheduler = new ApelScheduler();
-	public static List<DelayedTask<Runnable>> delayedTasks = new ArrayList<>();
+	public static ExecutorService drawThread = Executors.newSingleThreadExecutor();
 
 	@Override
 	public void onInitialize() {
