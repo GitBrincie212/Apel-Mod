2475f9552d1bf22d90979da3ef028ba4e615a144
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2024-06-02 16:06:22 +0300

    Was making a vector3f pool system(didn't work)

--- src/main/java/net/mcbrincie/apel/item/DebugParticleAnim1.java
+++ src/main/java/net/mcbrincie/apel/item/DebugParticleAnim1.java
@@ -1,10 +1,7 @@
 package net.mcbrincie.apel.item;
 
-import net.mcbrincie.apel.lib.animators.ParallelAnimator;
-import net.mcbrincie.apel.lib.animators.PathAnimatorBase;
 import net.mcbrincie.apel.lib.animators.PointAnimator;
-import net.mcbrincie.apel.lib.objects.ParticleCuboid;
-import net.mcbrincie.apel.lib.util.interceptor.InterceptedResult;
+import net.mcbrincie.apel.lib.objects.ParticleSphere;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -13,11 +10,7 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3f;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class DebugParticleAnim1 extends Item {
     public DebugParticleAnim1(Settings settings) {
@@ -33,53 +26,20 @@ public class DebugParticleAnim1 extends Item {
     }
 
     private void animators(ServerWorld world) {
-        ParticleCuboid cuboid1 = new ParticleCuboid(
-                ParticleTypes.END_ROD, 50,
-                new Vector3f(2f, 2f, 2f)
+        ParticleSphere sphere1 = new ParticleSphere(
+                ParticleTypes.END_ROD, 6.5f,
+                new Vector3f(), 5000
         );
-
-        ParticleCuboid cuboid2 = new ParticleCuboid(cuboid1);
-        cuboid2.setParticleEffect(ParticleTypes.FLAME);
-        cuboid2.setSize(new Vector3f(4.5f, 4.5f, 4.5f));
-
-        ParticleCuboid cuboid3 = new ParticleCuboid(cuboid1);
-        cuboid3.setSize(new Vector3f(6f, 6f, 6f));
-        cuboid3.setParticleEffect(ParticleTypes.SOUL_FIRE_FLAME);
-
-        cuboid1.beforeCalcsIntercept = (data, obj) -> {
-            obj.setRotation(obj.getRotation().add(0.002f, 0, 0));
-            return new InterceptedResult<>(data, obj);
-        };
-        cuboid2.beforeCalcsIntercept = (data, obj) -> {
-            obj.setRotation(obj.getRotation().add(0, 0, 0.004f));
-            return new InterceptedResult<>(data, obj);
-        };
-        cuboid3.beforeCalcsIntercept = (data, obj) -> {
-            obj.setRotation(obj.getRotation().add(0, 0.008f, 0));
-            return new InterceptedResult<>(data, obj);
-        };
-        ParallelAnimator animator = getParallelAnimator(cuboid1, cuboid2, cuboid3);
-        animator.beginAnimation(world);
-    }
-
-    private static @NotNull ParallelAnimator getParallelAnimator(ParticleCuboid cuboid1, ParticleCuboid cuboid2, ParticleCuboid cuboid3) {
+        ParticleSphere sphere2 = new ParticleSphere(sphere1);
+        sphere2.setRadius(4.5f);
+        sphere2.setParticleEffect(ParticleTypes.FLAME);
         PointAnimator pointAnimator1 = new PointAnimator(
-                1, cuboid1, new Vector3f(), 1000
+                1, sphere1, new Vector3f(), 1000
         );
         PointAnimator pointAnimator2 = new PointAnimator(
-                1, cuboid2, new Vector3f(), 1000
+                2, sphere2, new Vector3f(), 1000
         );
-        PointAnimator pointAnimator3 = new PointAnimator(
-                1, cuboid3, new Vector3f(), 1000
-        );
-        List<Integer> delays = new ArrayList<>();
-        List<PathAnimatorBase> pathAnimatorList = new ArrayList<>();
-        pathAnimatorList.add(pointAnimator1);
-        pathAnimatorList.add(pointAnimator2);
-        pathAnimatorList.add(pointAnimator3);
-        delays.add(0);
-        delays.add(20);
-        delays.add(60);
-        return new ParallelAnimator(delays, pathAnimatorList);
+        pointAnimator1.beginAnimation(world);
+        pointAnimator2.beginAnimation(world);
     }
 }
