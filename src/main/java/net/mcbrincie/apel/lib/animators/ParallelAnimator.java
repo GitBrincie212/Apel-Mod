7ae46456a9470348846fc3236a02a7a1ef8fdd3c
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2024-06-02 13:43:08 +0300

    Removed CombinativeAnimator and currently replacing with the ParallelAnimator & SequentialAnimator(WIP)

---
+++ src/main/java/net/mcbrincie/apel/lib/animators/ParallelAnimator.java
@@ -0,0 +1,124 @@
+package net.mcbrincie.apel.lib.animators;
+
+import net.mcbrincie.apel.Apel;
+import net.mcbrincie.apel.lib.exceptions.SeqDuplicateException;
+import net.mcbrincie.apel.lib.exceptions.SeqMissingException;
+import net.mcbrincie.apel.lib.objects.ParticleObject;
+import net.mcbrincie.apel.lib.util.scheduler.ScheduledStep;
+import net.minecraft.server.world.ServerWorld;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@SuppressWarnings("unused")
+public class ParallelAnimator extends PathAnimatorBase {
+    protected List<PathAnimatorBase> animators = new ArrayList<>();
+    protected List<Integer> delays = new ArrayList<>();
+
+    public ParallelAnimator(int delay, PathAnimatorBase... pathAnimators) {
+        super(delay, null, pathAnimators.length);
+        if (pathAnimators.length == 0) {
+            throw new IllegalArgumentException("There must be at least one path animator");
+        }
+        this.animators.addAll(List.of(pathAnimators));
+    }
+
+    public ParallelAnimator(int delay, List<PathAnimatorBase> pathAnimators) {
+        super(delay, null, pathAnimators.size());
+        if (pathAnimators.isEmpty()) {
+            throw new IllegalArgumentException("There must be at least one path animator");
+        }
+        this.animators.addAll(pathAnimators);
+    }
+
+    public ParallelAnimator(List<Integer> delay, List<PathAnimatorBase> pathAnimators) {
+        super(0, null, pathAnimators.size());
+        this.delay = -1;
+        if (pathAnimators.isEmpty()) {
+            throw new IllegalArgumentException("There must be at least one path animator");
+        }
+        if (pathAnimators.size() != delay.size()) {
+            throw new IllegalArgumentException("Delays must match the number of path animators");
+        }
+        this.animators.addAll(pathAnimators);
+        this.delays.addAll(delay);
+    }
+
+    public ParallelAnimator(List<Integer> delay, PathAnimatorBase... pathAnimators) {
+        super(0, null, pathAnimators.length);
+        this.delay = -1;
+        if (pathAnimators.length == 0) {
+            throw new IllegalArgumentException("There must be at least one path animator");
+        }
+        if (pathAnimators.length != delay.size()) {
+            throw new IllegalArgumentException("Delays must match the number of path animators");
+        }
+        this.animators.addAll(List.of(pathAnimators));
+        this.delays.addAll(delay);
+    }
+
+    public void addAnimatorPath(PathAnimatorBase animator) {
+        this.animators.add(animator);
+    }
+
+    public void removeAnimatorPath(PathAnimatorBase animator) {
+        this.animators.remove(animator);
+    }
+
+    @Override
+    @Deprecated
+    public int setRenderSteps(int steps) {
+        return -1;
+    }
+
+    @Deprecated
+    @Override
+    public ParticleObject setParticleObject(@NotNull ParticleObject object) {
+        return null;
+    }
+
+    @Override
+    @Deprecated
+    public float setRenderInterval(float interval) {
+        return -1.0f;
+    }
+
+    @Override
+    public int convertToSteps() {
+        return this.animators.size();
+    }
+
+    @Override
+    public void beginAnimation(ServerWorld world) throws SeqDuplicateException, SeqMissingException {
+        this.allocateToScheduler();
+        int step = 0;
+        for (PathAnimatorBase animator : this.animators) {
+            step++;
+            this.allocateNewAnimator(step, world, animator);
+        }
+    }
+
+    public void allocateNewAnimator(int step, ServerWorld world, PathAnimatorBase animator) {
+        Runnable func = () -> animator.beginAnimation(world);
+        int delayUsed = (this.delay == -1) ? this.delays.get(step - 1) : this.delay;
+        if (delayUsed == 0) {
+            Apel.drawThread.submit(func);
+            return;
+        }
+        if (this.processSpeed <= 1) {
+            Apel.apelScheduler.allocateNewStep(
+                    this, new ScheduledStep(delayUsed, new Runnable[]{func})
+            );
+            return;
+        } else if (step % this.processSpeed != 0) {
+            this.storedFuncsBuffer.add(func);
+            return;
+        }
+        Apel.apelScheduler.allocateNewStep(
+                this, new ScheduledStep(delayUsed, this.storedFuncsBuffer.toArray(Runnable[]::new))
+        );
+        this.storedFuncsBuffer.clear();
+    }
+}
