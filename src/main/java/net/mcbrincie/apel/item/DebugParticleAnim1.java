90236bcbb0cc77c1845d24bcb5851f602589ebc8
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2024-06-05 10:16:12 +0300

    Updated DebugParticleAnim1.java
    Updated DebugParticleAnim1 to not feature any animation
    
    Signed-off-by: McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>

--- src/main/java/net/mcbrincie/apel/item/DebugParticleAnim1.java
+++ src/main/java/net/mcbrincie/apel/item/DebugParticleAnim1.java
@@ -27,71 +27,7 @@ public class DebugParticleAnim1 extends Item {
         return TypedActionResult.pass(user.getMainHandStack());
     }
 
-    private void animators(ServerWorld world) {
-        ParticleCuboid cuboid1 = new ParticleCuboid(
-                ParticleTypes.END_ROD, 50,
-                new Vector3f(2f, 2f, 2f)
-        );
-
-        ParticleCuboid cuboid2 = new ParticleCuboid(cuboid1);
-        cuboid2.setParticleEffect(ParticleTypes.FLAME);
-        cuboid2.setSize(new Vector3f(4.5f, 4.5f, 4.5f));
-
-        ParticleCuboid cuboid3 = new ParticleCuboid(cuboid1);
-        cuboid3.setSize(new Vector3f(6f, 6f, 6f));
-        cuboid3.setParticleEffect(ParticleTypes.SOUL_FIRE_FLAME);
-
-        ParticleCuboid cuboid4 = new ParticleCuboid(cuboid1);
-        cuboid4.setParticleEffect(ParticleTypes.COMPOSTER);
-        cuboid4.setSize(new Vector3f(7.5f, 7.5f, 7.5f));
-
-        ParticleCuboid cuboid5 = new ParticleCuboid(cuboid1);
-        cuboid5.setSize(new Vector3f(9f, 9f, 9f));
-        cuboid5.setParticleEffect(ParticleTypes.ENCHANTED_HIT);
-
-        ParticleCuboid cuboid6 = new ParticleCuboid(cuboid1);
-        cuboid6.setSize(new Vector3f(11f, 11f, 11f));
-        cuboid6.setParticleEffect(ParticleTypes.DRAGON_BREATH);
-
-        ParticleCombiner<ParticleCuboid> combinedObj = new ParticleCombiner<>(cuboid1, cuboid2, cuboid3);
-        ParticleCombiner<ParticleCuboid> combinedObj2 = new ParticleCombiner<>(cuboid4, cuboid5, cuboid6);
-
-        combinedObj.beforeChildRenderIntercept = (data, obj) -> {
-            ParticleCuboid cuboidInUse = (ParticleCuboid) data.getMetadata(
-                    ParticleCombiner.beforeChildRenderData.OBJECT_IN_USE
-            );
-            Vector3f rot = new Vector3f();
-            if (cuboidInUse.equals(cuboid1)) rot.add(0.002f, 0, 0);
-            else if (cuboidInUse.equals(cuboid2)) rot.add(0, 0, 0.004f);
-            else if (cuboidInUse.equals(cuboid3)) rot.add(0, 0.008f, 0);
-            cuboidInUse.setRotation(cuboidInUse.getRotation().add(rot));
-            return new InterceptedResult<>(data, obj);
-        };
-
-        combinedObj2.beforeChildRenderIntercept = (data, obj) -> {
-            ParticleCuboid cuboidInUse = (ParticleCuboid) data.getMetadata(
-                    ParticleCombiner.beforeChildRenderData.OBJECT_IN_USE
-            );
-            Vector3f rot = new Vector3f();
-            if (cuboidInUse.equals(cuboid4)) rot.add(0.002f, 0, 0.002f);
-            else if (cuboidInUse.equals(cuboid5)) rot.add(-0.004f, 0, 0.004f);
-            else if (cuboidInUse.equals(cuboid6)) rot.add(0.008f, 0, -0.008f);
-            cuboidInUse.setRotation(cuboidInUse.getRotation().add(rot));
-            return new InterceptedResult<>(data, obj);
-        };
-
-        ParticleCombiner<?> wholeObj = new ParticleCombiner<>(combinedObj, combinedObj2);
-
-        wholeObj.afterChildRenderIntercept = (data, obj) -> {
-            if (data.currentStep >= 200 && data.currentStep <= 210) {
-                obj.setParticleEffectRecursively(ParticleTypes.CRIT);
-            }
-            return new InterceptedResult<>(data, obj);
-        };
-
-        PointAnimator pointAnimator1 = new PointAnimator(
-                1, wholeObj, new Vector3f(), 1000
-        );
-        pointAnimator1.beginAnimation(world);
+    private void animators(ServerWorld world) {`
+       // ....
     }
 }
