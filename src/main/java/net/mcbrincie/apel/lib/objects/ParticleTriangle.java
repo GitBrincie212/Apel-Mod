7104b630ecedc2b8618b6798d2c433c6e9c13087
DarthSharkie <darthsharkie@gmail.com>
2024-06-11 01:22:23 -0700

    Standardize ParticleTriangle
    This is a follow-up to https://github.com/GitBrincie212/Apel-Mod/pull/1
    to standardize on beforeDraw, afterDraw, drawPos, and particleEffect.
    Field visibility of DrawInterceptor implementations is adjusted and set
    via setter to ensure the correct interceptor is present and called, if
    present.
    
    This also telescopes the constructor so logic isn't repeated in
    constructors. This follows a best practice to have one constructor and
    one copy constructor per class.
    
    Define, default, and document before/after interceptors.
    
    Add rotation/offset logic to match other `ParticleObject` subclasses.
    
    Fix a vertex bug where `vertex2` was referred to extra times.
    
    Fix a validation bug where the cross-product modified `vertex1`
    in-place.

--- src/main/java/net/mcbrincie/apel/lib/objects/ParticleTriangle.java
+++ src/main/java/net/mcbrincie/apel/lib/objects/ParticleTriangle.java
@@ -8,19 +8,22 @@ import net.minecraft.particle.ParticleEffect;
 import net.minecraft.server.world.ServerWorld;
 import org.joml.Vector3f;
 
+import java.util.Optional;
+
 /** The particle object class that represents a 2D triangle. It has 3
  * vertices that make it up, all of them must be coplanar with each other.
  * The vertices can be set individually or by supplying a list of 4 vertices
 */
 @SuppressWarnings({"unused", "UnusedReturnValue"})
 public class ParticleTriangle extends ParticleObject {
-    public DrawInterceptor<ParticleTriangle, emptyData> afterCalcsIntercept;
-    public DrawInterceptor<ParticleTriangle, emptyData> beforeCalcsIntercept;
+    private DrawInterceptor<ParticleTriangle, AfterDrawData> afterDraw = DrawInterceptor.identity();
+    private DrawInterceptor<ParticleTriangle, BeforeDrawData> beforeDraw = DrawInterceptor.identity();
 
     private final CommonUtils commonUtils = new CommonUtils();
 
     /** There is no data being transmitted */
-    public enum emptyData {}
+    public enum BeforeDrawData {}
+    public enum AfterDrawData {}
 
     protected Vector3f vertex1;
     protected Vector3f vertex2;
@@ -34,15 +37,15 @@ public class ParticleTriangle extends ParticleObject {
      * the particle to use, the vertices that connect the triangle, the amount of particles & the
      * rotation to apply There is also a simplified version for no rotation.
      *
-     * @param particle The particle to use
+     * @param particleEffect The particle to use
      * @param amount The amount of particles for the object
      * @param vertices The vertices that make up the triangle
      * @param rotation The rotation to apply
      *
      * @see ParticleTriangle#ParticleTriangle(ParticleEffect, Vector3f[], int)
     */
-    public ParticleTriangle(ParticleEffect particle, Vector3f[] vertices, int amount, Vector3f rotation) {
-        super(particle, rotation);
+    public ParticleTriangle(ParticleEffect particleEffect, Vector3f[] vertices, int amount, Vector3f rotation) {
+        super(particleEffect, rotation);
         if (vertices.length != 3) {
             throw UNBALANCED_VERTICES;
         }
@@ -58,22 +61,14 @@ public class ParticleTriangle extends ParticleObject {
      * It is a simplified version for the case when no rotation is meant to be applied.
      * For rotation offset you can use another constructor
      *
-     * @param particle The particle to use
+     * @param particleEffect The particle to use
      * @param vertices The vertices that make up the triangle
      * @param amount The amount of particles for the object
      *
      * @see ParticleTriangle#ParticleTriangle(ParticleEffect, Vector3f[], int, Vector3f)
     */
-    public ParticleTriangle(ParticleEffect particle, Vector3f[] vertices, int amount) {
-        super(particle);
-        if (vertices.length != 3) {
-            throw UNBALANCED_VERTICES;
-        }
-        this.checkValidTriangle(vertices);
-        this.vertex1 = vertices[0];
-        this.vertex2 = vertices[1];
-        this.vertex3 = vertices[2];
-        this.amount = amount;
+    public ParticleTriangle(ParticleEffect particleEffect, Vector3f[] vertices, int amount) {
+        this(particleEffect, vertices, amount, new Vector3f(0));
     }
 
     /** The copy constructor for a specific particle object. It copies all
@@ -86,12 +81,13 @@ public class ParticleTriangle extends ParticleObject {
         this.vertex1 = triangle.vertex1;
         this.vertex2 = triangle.vertex2;
         this.vertex3 = triangle.vertex3;
-        this.beforeCalcsIntercept = triangle.beforeCalcsIntercept;
-        this.afterCalcsIntercept = triangle.afterCalcsIntercept;
+        this.beforeDraw = triangle.beforeDraw;
+        this.afterDraw = triangle.afterDraw;
     }
 
     private void checkValidTriangle(Vector3f vertex1, Vector3f vertex2, Vector3f vertex3) {
-        float dotProduct1 = vertex3.dot(vertex1.cross(vertex2));
+        // Defensive copy before cross-product, which is done in-place.
+        float dotProduct1 = vertex3.dot(new Vector3f(vertex1).cross(vertex2));
         if (dotProduct1 == 0) {
             throw new IllegalArgumentException("Provided vertices do not produce a triangle");
         }
@@ -131,7 +127,7 @@ public class ParticleTriangle extends ParticleObject {
      */
     public Vector3f setVertex2(Vector3f newVertex) {
         Vector3f prevVertex2 = this.vertex2;
-        this.checkValidTriangle(this.vertex2, vertex2, this.vertex3);
+        this.checkValidTriangle(this.vertex1, vertex2, this.vertex3);
         this.vertex2 = newVertex;
         return prevVertex2;
     }
@@ -190,33 +186,47 @@ public class ParticleTriangle extends ParticleObject {
 
     @Override
     public void draw(ServerWorld world, int step, Vector3f drawPos) {
-        InterceptedResult<ParticleTriangle, emptyData> modifiedBefore =
-                this.interceptDrawCalcBefore(world, step, drawPos, this);
-        ParticleTriangle objectToUse = modifiedBefore.object;
-        drawPos = drawPos.add(this.offset);
-        Vector3f vertex1 = this.vertex1.add(drawPos);
-        Vector3f vertex2 = this.vertex2.add(drawPos);
-        Vector3f vertex3 = this.vertex3.add(drawPos);
-        commonUtils.drawLine(objectToUse, world, vertex1, vertex2, objectToUse.amount);
-        commonUtils.drawLine(objectToUse, world, vertex2, vertex3, objectToUse.amount);
-        commonUtils.drawLine(objectToUse, world, vertex3, vertex1, objectToUse.amount);
-        this.interceptDrawCalcAfter(world, step, drawPos, this);
+        this.doBeforeDraw(world, step, drawPos);
+        // Defensive copy of `drawPos`
+        Vector3f totalOffset = new Vector3f(drawPos).add(this.offset);
+        // Defensive copies of internal vertices
+        Vector3f vertex1 = new Vector3f(this.vertex1).rotateZ(this.rotation.z).rotateY(this.rotation.y).rotateX(this.rotation.x).add(totalOffset);
+        Vector3f vertex2 = new Vector3f(this.vertex2).rotateZ(this.rotation.z).rotateY(this.rotation.y).rotateX(this.rotation.x).add(totalOffset);
+        Vector3f vertex3 = new Vector3f(this.vertex3).rotateZ(this.rotation.z).rotateY(this.rotation.y).rotateX(this.rotation.x).add(totalOffset);
+        commonUtils.drawLine(this, world, vertex1, vertex2, this.amount);
+        commonUtils.drawLine(this, world, vertex2, vertex3, this.amount);
+        commonUtils.drawLine(this, world, vertex3, vertex1, this.amount);
+        this.doAfterDraw(world, step, drawPos);
         this.endDraw(world, step, drawPos);
     }
 
-    private InterceptedResult<ParticleTriangle, emptyData> interceptDrawCalcAfter(
-            ServerWorld world, int step, Vector3f pos, ParticleTriangle obj
-    ) {
-        InterceptData<emptyData> interceptData = new InterceptData<>(world, pos, step, emptyData.class);
-        if (this.afterCalcsIntercept == null) return new InterceptedResult<>(interceptData, this);
-        return this.afterCalcsIntercept.apply(interceptData, obj);
+    /** Set the interceptor to run after drawing the triangle.  The interceptor will be provided
+     * with references to the {@link ServerWorld}, the position where the triangle is rendered, and the
+     * step number of the animation.  There is no other data attached.
+     *
+     * @param afterDraw the new interceptor to execute prior to drawing the triangle
+     */
+    public void setAfterDraw(DrawInterceptor<ParticleTriangle, AfterDrawData> afterDraw) {
+        this.afterDraw = Optional.ofNullable(afterDraw).orElse(DrawInterceptor.identity());
     }
 
-    private InterceptedResult<ParticleTriangle, emptyData> interceptDrawCalcBefore(
-            ServerWorld world, int step, Vector3f pos, ParticleTriangle obj
-    ) {
-        InterceptData<emptyData> interceptData = new InterceptData<>(world, pos, step, emptyData.class);
-        if (this.beforeCalcsIntercept == null) return new InterceptedResult<>(interceptData, this);
-        return this.beforeCalcsIntercept.apply(interceptData, obj);
+    private InterceptedResult<ParticleTriangle, AfterDrawData> doAfterDraw(ServerWorld world, int step, Vector3f pos) {
+        InterceptData<AfterDrawData> interceptData = new InterceptData<>(world, pos, step, AfterDrawData.class);
+        return this.afterDraw.apply(interceptData, this);
+    }
+
+    /** Set the interceptor to run prior to drawing the triangle.  The interceptor will be provided
+     * with references to the {@link ServerWorld}, the position where the triangle is rendered, and the
+     * step number of the animation.  There is no other data attached.
+     *
+     * @param beforeDraw the new interceptor to execute prior to drawing the triangle
+     */
+    public void setBeforeDraw(DrawInterceptor<ParticleTriangle, BeforeDrawData> beforeDraw) {
+        this.beforeDraw = Optional.ofNullable(beforeDraw).orElse(DrawInterceptor.identity());
+    }
+
+    private InterceptedResult<ParticleTriangle, BeforeDrawData> doBeforeDraw(ServerWorld world, int step, Vector3f pos) {
+        InterceptData<BeforeDrawData> interceptData = new InterceptData<>(world, pos, step, BeforeDrawData.class);
+        return this.beforeDraw.apply(interceptData, this);
     }
 }
