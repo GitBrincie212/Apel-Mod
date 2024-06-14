e3767130110c3b95d2caea971fce908d1439e9ab
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2024-06-14 22:42:41 +0300

    Fixed a bug that causes the animation to play in reverse

--- src/main/java/net/mcbrincie/apel/lib/util/scheduler/ScheduledSequence.java
+++ src/main/java/net/mcbrincie/apel/lib/util/scheduler/ScheduledSequence.java
@@ -1,22 +1,23 @@
 package net.mcbrincie.apel.lib.util.scheduler;
 
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class ScheduledSequence {
-    private final Stack<ScheduledStep> scheduledSteps;
+    private final Queue<ScheduledStep> scheduledSteps;
     public boolean hasAllocatedOnce = false;
 
     public ScheduledSequence() {
-        this.scheduledSteps = new Stack<>();
+        this.scheduledSteps = new LinkedList<>();
     }
 
     public void allocateStep(ScheduledStep step) {
         this.hasAllocatedOnce = true;
-        this.scheduledSteps.push(step);
+        this.scheduledSteps.add(step);
     }
 
     public void deallocateStep() {
-        this.scheduledSteps.pop();
+        this.scheduledSteps.remove();
     }
 
     public boolean isEmpty() {
