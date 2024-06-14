2b435fbc49967c3dcd1818a95d13718641caa054
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2024-06-14 22:44:12 +0300

    Added caching for the palate generator

--- src/main/java/net/mcbrincie/apel/lib/util/image/DustPalateGenerator.java
+++ src/main/java/net/mcbrincie/apel/lib/util/image/DustPalateGenerator.java
@@ -5,11 +5,16 @@ import net.minecraft.particle.ParticleEffect;
 import net.minecraft.util.math.Vec3d;
 import org.joml.Vector3f;
 
+import java.util.HashMap;
+
 public class DustPalateGenerator implements PalateGenerator {
+    private static final HashMap<Integer, DustParticleEffect> cache = new HashMap<>();
+
     @Override
     public ParticleEffect apply(int rgba, int x, int y, Vector3f position) {
-        Vector3f color = Vec3d.unpackRgb(rgba).toVector3f();
-        DustParticleEffect dust = new DustParticleEffect(color, 0.1f);
-        return dust;
+        return cache.computeIfAbsent(rgba, k -> {
+            Vector3f color = Vec3d.unpackRgb(rgba).toVector3f();
+            return new DustParticleEffect(color, 0.2f);
+        });
     }
 }
