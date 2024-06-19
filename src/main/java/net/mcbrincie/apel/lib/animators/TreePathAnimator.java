f4a705396070b8adfa731878c562b258fb651fc4
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2024-06-19 16:37:10 +0300

    Added a tree path animator interface for animators that need a tree-like function

--- src/main/java/net/mcbrincie/apel/lib/animators/TreePathAnimator.java
+++ src/main/java/net/mcbrincie/apel/lib/animators/TreePathAnimator.java
@@ -2,6 +2,14 @@ package net.mcbrincie.apel.lib.animators;
 
 import java.util.List;
 
+/** The TreePathAnimator is an interface for path animators that define a tree-like structure,
+ * this tree-like structure is composed of multiple path animator(s) as children(s).
+ * Parallel and Sequential path animator uses this interface if you plan to make something
+ * similar, you should also use it
+ *
+ * @param <T> The path animator to host
+ */
+@SuppressWarnings("unused")
 public interface TreePathAnimator<T extends PathAnimatorBase> {
     void addAnimatorPath(PathAnimatorBase animator);
     void removeAnimatorPath(PathAnimatorBase animator);
