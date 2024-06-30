0000a5d59436552883963f0754ecd33f55fdebb4
DarthSharkie <darthsharkie@gmail.com>
2024-06-29 18:16:22 -0700

    Correct cubic Bézier point computation
    It had an extra (1-t) power on the $ term.

--- src/test/java/net/mcbrincie/apel/lib/util/math/bezier/BezierCurveTest.java
+++ src/test/java/net/mcbrincie/apel/lib/util/math/bezier/BezierCurveTest.java
@@ -36,4 +36,25 @@ class BezierCurveTest {
         assertEquals(bc1, bc2);
         assertEquals(bc1.hashCode(), bc2.hashCode());
     }
+
+    @Test
+    void testCubicBezierPoints() {
+        // Given a cubic Bézier curve
+        BezierCurve bc = BezierCurve.of(new Vector3f(0, 0, 5), new Vector3f(0, 5, 5),
+                                        List.of(new Vector3f(3, 2, 8), new Vector3f(-3, 3, 8))
+        );
+
+        // Verify points are within one-thousandth
+        assertTrue(new Vector3f(0f, 0f, 5f).equals(bc.compute(0.0f), 0.001f));
+        assertTrue(new Vector3f(0.648f, 0.572f, 5.810f).equals(bc.compute(0.1f), 0.001f));
+        assertTrue(new Vector3f(0.864f, 1.096f, 6.44f).equals(bc.compute(0.2f), 0.001f));
+        assertTrue(new Vector3f(0.756f, 1.584f, 6.890f).equals(bc.compute(0.3f), 0.001f));
+        assertTrue(new Vector3f(0.432f, 2.048f, 7.160f).equals(bc.compute(0.4f), 0.001f));
+        assertTrue(new Vector3f(0.0f, 2.5f, 7.25f).equals(bc.compute(0.5f), 0.001f));
+        assertTrue(new Vector3f(-0.432f, 2.952f, 7.160f).equals(bc.compute(0.6f), 0.001f));
+        assertTrue(new Vector3f(-0.756f, 3.416f, 6.890f).equals(bc.compute(0.7f), 0.001f));
+        assertTrue(new Vector3f(-0.864f, 3.904f, 6.440f).equals(bc.compute(0.8f), 0.001f));
+        assertTrue(new Vector3f(-0.648f, 4.428f, 5.810f).equals(bc.compute(0.9f), 0.001f));
+        assertTrue(new Vector3f(0.0f, 5.0f, 5.0f).equals(bc.compute(1.0f), 0.001f));
+    }
 }
