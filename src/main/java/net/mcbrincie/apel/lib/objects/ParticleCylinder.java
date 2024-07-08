b09e07b93946d2cbd1c222d858308ef382c34a30
DarthSharkie <darthsharkie@gmail.com>
2024-07-07 23:00:22 -0700

    Convert ParticleCylinder to builder
    Extend ParticleObject.Builder and make constructor-called setters final.

--- src/main/java/net/mcbrincie/apel/lib/objects/ParticleCylinder.java
+++ src/main/java/net/mcbrincie/apel/lib/objects/ParticleCylinder.java
@@ -3,9 +3,7 @@ package net.mcbrincie.apel.lib.objects;
 import net.mcbrincie.apel.lib.renderers.ApelServerRenderer;
 import net.mcbrincie.apel.lib.util.interceptor.DrawInterceptor;
 import net.mcbrincie.apel.lib.util.interceptor.InterceptData;
-import net.minecraft.particle.ParticleEffect;
 import net.minecraft.server.world.ServerWorld;
-import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3f;
 
 import java.util.Optional;
@@ -23,8 +21,8 @@ public class ParticleCylinder extends ParticleObject {
     protected float radius;
     protected float height;
 
-    private DrawInterceptor<ParticleCylinder, AfterDrawData> afterDraw = DrawInterceptor.identity();
-    private DrawInterceptor<ParticleCylinder, BeforeDrawData> beforeDraw = DrawInterceptor.identity();
+    private DrawInterceptor<ParticleCylinder, AfterDrawData> afterDraw;
+    private DrawInterceptor<ParticleCylinder, BeforeDrawData> beforeDraw;
 
     /** This data is used before calculations (it contains the iterated rotation) */
     public enum BeforeDrawData {}
@@ -32,49 +30,16 @@ public class ParticleCylinder extends ParticleObject {
     /** This data is used after calculations (it contains the drawing position) */
     public enum AfterDrawData {}
 
-    /** Constructor for the particle cylinder. It accepts as parameters the particle effect to use, the radius of the
-     * cylinder, the height of the cylinder, the rotation to apply, and the number of particles.
-     *
-     * <p>This implementation calls setters for amount, rotation, height, and radius so checks are performed to
-     * ensure valid values are accepted for each property.  Subclasses should take care not to violate these lest
-     * they risk undefined behavior.
-     *
-     * @param particleEffect The particle to use
-     * @param amount The number of particles for the object
-     * @param radius The radius of the cylinder (how xz wise big it is)
-     * @param height The height of the cylinder (how tall it is)
-     * @param rotation The rotation to apply
-     *
-     * @see ParticleCylinder#ParticleCylinder(ParticleEffect, float, float, int)
-    */
-    public ParticleCylinder(
-            @NotNull ParticleEffect particleEffect, float radius, float height, Vector3f rotation, int amount
-    ) {
-        super(particleEffect, rotation);
-        this.setRadius(radius);
-        this.setAmount(amount);
-        this.setHeight(height);
+    public static Builder<?> builder() {
+        return new Builder<>();
     }
 
-    /** Constructor for the particle cylinder. It accepts as parameters the particle effect to use, the radius of the
-     * cylinder, the height of the cylinder, and the number of particles.
-     *
-     * <p>This implementation calls setters for amount, rotation, height, and radius so checks are performed to
-     * ensure valid values are accepted for each property.  Subclasses should take care not to violate these lest
-     * they risk undefined behavior.
-     *
-     * @param particleEffect The particle to use
-     * @param amount The number of particles for the object
-     * @param height The height of the cylinder (how tall it is)
-     * @param radius The radius of the cylinder (how xz wise big it is)
-     *
-     * @see ParticleCylinder#ParticleCylinder(ParticleEffect, float, float, Vector3f, int)
-    */
-    public ParticleCylinder(
-            @NotNull ParticleEffect particleEffect,
-            float radius, float height, int amount
-    ) {
-        this(particleEffect, radius, height, new Vector3f(0,0,0), amount);
+    private ParticleCylinder(Builder<?> builder) {
+        super(builder.particleEffect, builder.rotation, builder.offset, builder.amount);
+        this.setRadius(builder.radius);
+        this.setHeight(builder.height);
+        this.setBeforeDraw(builder.beforeDraw);
+        this.setAfterDraw(builder.afterDraw);
     }
 
     /** The copy constructor for a specific particle object. It copies all
@@ -86,7 +51,6 @@ public class ParticleCylinder extends ParticleObject {
         super(cylinder);
         this.radius = cylinder.radius;
         this.height = cylinder.height;
-        this.amount = cylinder.amount;
         this.afterDraw = cylinder.afterDraw;
         this.beforeDraw = cylinder.beforeDraw;
     }
@@ -99,13 +63,16 @@ public class ParticleCylinder extends ParticleObject {
         return radius;
     }
 
-    /** Set the radius of this ParticleCylinder and returns the previous radius that was used.  Radius must be positive.
+    /**
+     * Set the radius of this ParticleCylinder and returns the previous radius that was used.  Radius must be positive.
+     * <p>
+     * This implementation is used by the constructor, so subclasses cannot override this method.
      *
      * @param radius the new radius
      * @return the previously used radius
      */
-    public float setRadius(float radius) {
-        if (radius < 0) {
+    public final float setRadius(float radius) {
+        if (radius <= 0) {
             throw new IllegalArgumentException("Radius must be positive");
         }
         float prevRadius = this.radius;
@@ -121,13 +88,16 @@ public class ParticleCylinder extends ParticleObject {
         return height;
     }
 
-    /** Sets the height of the ParticleCylinder and returns the previous height that was used.  Height must be positive.
+    /**
+     * Sets the height of the ParticleCylinder and returns the previous height that was used.  Height must be positive.
+     * <p>
+     * This implementation is used by the constructor, so subclasses cannot override this method.
      *
      * @param height The new height
      * @return The previous used height
      */
-    public float setHeight(float height) {
-        if (height < 0) {
+    public final float setHeight(float height) {
+        if (height <= 0) {
             throw new IllegalArgumentException("Height must be positive");
         }
         float prevHeight = this.height;
@@ -144,13 +114,16 @@ public class ParticleCylinder extends ParticleObject {
         this.endDraw(renderer, step, drawPos);
     }
 
-    /** Set the interceptor to run after drawing the cylinder. The interceptor will be provided
+    /**
+     * Set the interceptor to run after drawing the cylinder. The interceptor will be provided
      * with references to the {@link ServerWorld}, the step number of the animation, and the
      * position where the cylinder is rendered.
+     * <p>
+     * This implementation is used by the constructor, so subclasses cannot override this method.
      *
      * @param afterDraw the new interceptor to execute after drawing each particle
      */
-    public void setAfterDraw(DrawInterceptor<ParticleCylinder, AfterDrawData> afterDraw) {
+    public final void setAfterDraw(DrawInterceptor<ParticleCylinder, AfterDrawData> afterDraw) {
         this.afterDraw = Optional.ofNullable(afterDraw).orElse(DrawInterceptor.identity());
     }
 
@@ -159,18 +132,73 @@ public class ParticleCylinder extends ParticleObject {
         this.afterDraw.apply(interceptData, this);
     }
 
-    /** Set the interceptor to run prior to drawing the cylinder. The interceptor will be provided
+    /**
+     * Set the interceptor to run prior to drawing the cylinder. The interceptor will be provided
      * with references to the {@link ServerWorld}, the step number of the animation, and the
      * position where the cylinder is rendered.
+     * <p>
+     * This implementation is used by the constructor, so subclasses cannot override this method.
      *
      * @param beforeDraw the new interceptor to execute prior to drawing each particle
      */
-    public void setBeforeDraw(DrawInterceptor<ParticleCylinder, BeforeDrawData> beforeDraw) {
+    public final void setBeforeDraw(DrawInterceptor<ParticleCylinder, BeforeDrawData> beforeDraw) {
         this.beforeDraw = Optional.ofNullable(beforeDraw).orElse(DrawInterceptor.identity());
     }
 
     private void doBeforeDraw(ServerWorld world, int step, Vector3f pos) {
         InterceptData<BeforeDrawData> interceptData = new InterceptData<>(world, pos, step, BeforeDrawData.class);
         this.beforeDraw.apply(interceptData, this);
+    }
+
+    public static class Builder<B extends Builder<B>> extends ParticleObject.Builder<B> {
+        protected float radius;
+        protected float height;
+        protected DrawInterceptor<ParticleCylinder, AfterDrawData> afterDraw;
+        protected DrawInterceptor<ParticleCylinder, BeforeDrawData> beforeDraw;
+
+        private Builder() {}
+
+        /**
+         * Set the height on the builder.  This method is not cumulative; repeated calls will overwrite the value.
+         */
+        public B height(float height) {
+            this.height = height;
+            return self();
+        }
+
+        /**
+         * Set the radius on the builder.  This method is not cumulative; repeated calls will overwrite the value.
+         */
+        public B radius(float radius) {
+            this.radius = radius;
+            return self();
+        }
+
+        /**
+         * Sets the interceptor to run after drawing.  This method is not cumulative; repeated calls will overwrite
+         * the value.
+         *
+         * @see ParticleCylinder#setAfterDraw(DrawInterceptor)
+         */
+        public B afterDraw(DrawInterceptor<ParticleCylinder, AfterDrawData> afterDraw) {
+            this.afterDraw = afterDraw;
+            return self();
+        }
+
+        /**
+         * Sets the interceptor to run before drawing.  This method is not cumulative; repeated calls will overwrite
+         * the value.
+         *
+         * @see ParticleCylinder#setBeforeDraw(DrawInterceptor)
+         */
+        public B beforeDraw(DrawInterceptor<ParticleCylinder, BeforeDrawData> beforeDraw) {
+            this.beforeDraw = beforeDraw;
+            return self();
+        }
+
+        @Override
+        public ParticleCylinder build() {
+            return null;
+        }
     }
 }
