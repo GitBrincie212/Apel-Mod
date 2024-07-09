d62a1cb705a51459057c795d64acd3362529e6f9
DarthSharkie <darthsharkie@gmail.com>
2024-07-08 18:21:01 -0700

    Convert ParticleCombiner to builder
    Combiner used to use `setRotation`, `setAmount`, and `setParticleEffect`
    to modify its child objects.  This is confusing, and incompatible with
    an approach that uses setters from the private constructor called by the
    Builder.  These methods also are now `final` in `ParticleObject`, so
    they cannot be overridden.
    
    Therefore, standardize on plural methods (`setAmounts`, `setOffsets`,
    `setParticleEffects`, and `setRotations`) to modify children.  Append
    "Recursively" to affect the entire combiner hierarchy.
    
    Some inconsistencies remain, but aren't relevant to adopting the builder
    pattern.  Rotations and amounts have offsets, while offset doesn't.
    This needs further attention for two reasons: 1) offsets could have
    offsets, and 2) nearly the same logic is repeated across these four
    properties.  For example, such the particle effect's recursive method
    accepts an array (should be a list?) of values for each successively
    deeper level in the hierarchy.  Rotation, amount, and offset could each
    do the same.  The return values also need to be reviewed - setting a
    hierarchy probably doesn't need to return anything, since such a list
    would reveal inner workings such as how the hierarchy traverses.
    
    Do not overload any superclass methods: none of those properties
    (particle effect, rotation, offset, or amount) currently apply to a
    combiner.

--- src/test/java/net/mcbrincie/apel/lib/objects/ParticleCombinerTest.java
+++ src/test/java/net/mcbrincie/apel/lib/objects/ParticleCombinerTest.java
@@ -1,5 +1,6 @@
 package net.mcbrincie.apel.lib.objects;
 
+import org.joml.Vector3f;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ParticleCombinerTest {
     // Use this to prevent having to initialize all the Minecraft Server logic
     private static final ParticlePoint.Builder<?> NULL_POINT_BUILDER = ParticlePoint.builder().particleEffect(null);
+    private static final float EPSILON = 1e-3f;
 
     @Test
     void setObjectsViaList() {
@@ -17,7 +19,7 @@ class ParticleCombinerTest {
         ParticlePoint p2 = NULL_POINT_BUILDER.build();
 
         // Given a ParticleCombiner
-        ParticleCombiner<ParticlePoint> combiner = new ParticleCombiner<>(p1, p2);
+        ParticleCombiner<ParticleObject> combiner = ParticleCombiner.builder().object(p1).object(p2).build();
 
         // Given points to append
         ParticlePoint p3 = NULL_POINT_BUILDER.build();
@@ -36,7 +38,7 @@ class ParticleCombinerTest {
         ParticlePoint p2 = NULL_POINT_BUILDER.build();
 
         // Given a ParticleCombiner
-        ParticleCombiner<ParticlePoint> combiner = new ParticleCombiner<>(p1, p2);
+        ParticleCombiner<ParticleObject> combiner = ParticleCombiner.builder().object(p1).object(p2).build();
 
         // Given a point to append
         ParticlePoint p3 = NULL_POINT_BUILDER.build();
@@ -56,7 +58,7 @@ class ParticleCombinerTest {
         ParticlePoint p2 = NULL_POINT_BUILDER.build();
 
         // Given a ParticleCombiner
-        ParticleCombiner<ParticlePoint> combiner = new ParticleCombiner<>(p1, p2);
+        ParticleCombiner<ParticleObject> combiner = ParticleCombiner.builder().object(p1).object(p2).build();
 
         // Given points to append
         ParticlePoint p3 = NULL_POINT_BUILDER.build();
@@ -67,5 +69,53 @@ class ParticleCombinerTest {
 
         // Then the combiner has four objects
         assertEquals(4, combiner.getObjects().size());
+    }
+
+    @Test
+    void testSetRotations() {
+        // Given a couple ParticlePoints
+        ParticlePoint p1 = NULL_POINT_BUILDER.build();
+        ParticlePoint p2 = NULL_POINT_BUILDER.build();
+        ParticlePoint p3 = NULL_POINT_BUILDER.build();
+
+        // Given a ParticleCombiner
+        ParticleCombiner<ParticleObject> combiner = ParticleCombiner.builder().object(p1).object(p2).object(p3).build();
+
+        // Given a rotation
+        Vector3f rotation = new Vector3f(0.1f);
+
+        // When the children's rotation is set
+        combiner.setRotations(rotation);
+
+        // Then each child object has the same rotation
+        assertEquals(rotation, combiner.getObject(0).getRotation());
+        assertEquals(rotation, combiner.getObject(1).getRotation());
+        assertEquals(rotation, combiner.getObject(2).getRotation());
+    }
+
+    @Test
+    void testSetRotationsWithOffset() {
+        // Given a couple ParticlePoints
+        ParticlePoint p1 = NULL_POINT_BUILDER.build();
+        ParticlePoint p2 = NULL_POINT_BUILDER.build();
+        ParticlePoint p3 = NULL_POINT_BUILDER.build();
+
+        // Given a ParticleCombiner
+        ParticleCombiner<ParticleObject> combiner = ParticleCombiner.builder().object(p1).object(p2).object(p3).build();
+
+        // Given a rotation
+        Vector3f rotation = new Vector3f(0.1f);
+
+        // When the children's rotation is set, with varying rotations per axis (to verify independence)
+        combiner.setRotations(rotation, 0.1f, 0.2f, 0.3f);
+
+        // Then each child object has the same rotation
+        assertVector3fEquals(rotation, combiner.getObject(0).getRotation());
+        assertVector3fEquals(new Vector3f(0.2f, 0.3f, 0.4f), combiner.getObject(1).getRotation());
+        assertVector3fEquals(new Vector3f(0.3f, 0.5f, 0.7f), combiner.getObject(2).getRotation());
+    }
+
+    private static void assertVector3fEquals(Vector3f expected, Vector3f actual) {
+        assertTrue(expected.equals(actual, EPSILON));
     }
 }
