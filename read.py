b784015d185602af15641fa615bdb24d1bf8c603
DarthSharkie <darthsharkie@gmail.com>
2024-07-31 00:39:59 -0500

    Add additional types to read.py
    This is helpful to dump a binary file into human-readable text, but
    it wasn't updated when new shapes were added.

--- read.py
+++ read.py
@@ -15,6 +15,18 @@ def read_file(f):
             print(f"P {struct.unpack('>fff', f.read(12))}")
         if ins == b'L':
             print(f"L {struct.unpack('>ffffffh', f.read(26))}")
+        if ins == b'E':
+            print(f"E {struct.unpack('>ffffffffh', f.read(34))}")
+        if ins == b'S':
+            print(f"S {struct.unpack('>fffffffffh', f.read(38))}")
+        if ins == b'B':
+            count = f.read(1)
+            print(f"B{count} {struct.unpack('>ffffff', f.read(24))} [{len(f.read((count - 2) * 12))}...] {struct.unpack('>ffffffh', f.read(26))}")
+        if ins == b'C':
+            print(f"C {struct.unpack('>ffffffffh', f.read(34))}")
+        if ins == b'Y':
+            print(f"Y {struct.unpack('>ffffffffh', f.read(34))}")
+
     f.close()
 
 
