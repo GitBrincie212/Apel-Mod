17382aac271319a97499daa2f10c843cfee7ce92
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-05-03 10:41:27 +0300

    Optimised Imports

--- src/main/java/net/mcbrincie/apel/item/DebugParticleWand2.java
+++ src/main/java/net/mcbrincie/apel/item/DebugParticleWand2.java
@@ -1,33 +1,16 @@
 package net.mcbrincie.apel.item;
 
-import net.mcbrincie.apel.lib.animators.BezierCurveAnimator;
-import net.mcbrincie.apel.lib.animators.ParallelAnimator;
-import net.mcbrincie.apel.lib.animators.PathAnimatorBase;
 import net.mcbrincie.apel.lib.animators.PointAnimator;
-import net.mcbrincie.apel.lib.objects.*;
+import net.mcbrincie.apel.lib.objects.ParticleBranchGen;
 import net.mcbrincie.apel.lib.renderers.ApelServerRenderer;
-import net.mcbrincie.apel.lib.util.math.bezier.QuadraticBezierCurve;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.joml.Vector3f;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class DebugParticleWand2 extends Item {
     public DebugParticleWand2(Settings settings) {
