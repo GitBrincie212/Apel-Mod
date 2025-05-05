6f6634b71ea92352b12f799bccabed59496ad76a
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-05-05 19:08:13 +0300

    Improved the ParticleBranchGen and made it functional. Now its just adding more parameters

--- src/main/java/net/mcbrincie/apel/lib/objects/ParticleBranchGen.java
+++ src/main/java/net/mcbrincie/apel/lib/objects/ParticleBranchGen.java
@@ -2,6 +2,7 @@ package net.mcbrincie.apel.lib.objects;
 
 import net.mcbrincie.apel.lib.renderers.ApelServerRenderer;
 import net.mcbrincie.apel.lib.util.interceptor.DrawContext;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
 import org.joml.Vector3f;
 
@@ -13,12 +14,12 @@ import org.joml.Vector3f;
 */
 @SuppressWarnings({"unused", "UnusedReturnValue"})
 public class ParticleBranchGen extends ParticleObject<ParticleBranchGen> {
-    protected Vector3f start;
-    protected Vector3f end;
-    protected Vector3f minLength;
-    protected Vector3f maxLength;
-    protected int subDivisions;
-    protected float taperIntensity;
+    protected Vector3f minAngle;
+    protected Vector3f maxAngle;
+    protected float minLength;
+    protected float maxLength;
+    protected int maxBranchCount;
+    protected int minBranchCount;
     private static final Random rand = Random.create();
 
     public static Builder<?> builder() {
@@ -28,11 +29,12 @@ public class ParticleBranchGen extends ParticleObject<ParticleBranchGen> {
     private ParticleBranchGen(Builder<?> builder) {
         super(builder.particleEffect, builder.rotation, builder.offset, builder.amount, builder.beforeDraw,
               builder.afterDraw);
-        this.setStart(builder.start);
-        this.setEnd(builder.end);
-        this.setMinLength(builder.minDimensionsThreshold);
-        this.setMaxLength(builder.maxDimensionsThreshold);
-        this.setSubDivisions(builder.subDivs);
+        this.setMaxBranches(builder.maxBranches);
+        this.setMinBranches(builder.minBranches);
+        this.setMinAngle(builder.minAngle);
+        this.setMaxAngle(builder.maxAngle);
+        this.setMinLength(builder.minLengthThreshold);
+        this.setMaxLength(builder.maxLengthThreshold);
     }
 
     /** The copy constructor for a specific particle object. It copies all
@@ -43,232 +45,251 @@ public class ParticleBranchGen extends ParticleObject<ParticleBranchGen> {
     */
     public ParticleBranchGen(ParticleBranchGen branchGen) {
         super(branchGen);
-        this.start = new Vector3f(branchGen.start);
-        this.end = new Vector3f(branchGen.end);
-        this.minLength = new Vector3f(branchGen.minLength);
-        this.maxLength = new Vector3f(branchGen.maxLength);
-        this.subDivisions = branchGen.subDivisions;
-        this.taperIntensity = branchGen.taperIntensity;
+        this.minAngle = new Vector3f(branchGen.minAngle);
+        this.maxAngle = new Vector3f(branchGen.maxAngle);
+        this.minLength = branchGen.minLength;
+        this.maxLength = branchGen.maxLength;
+        this.maxBranchCount = branchGen.maxBranchCount;
+        this.minBranchCount = branchGen.minBranchCount;
     }
 
-    /** Gets the minimum length threshold
+    /** Gets the minimum branch length threshold
      *
-     * @return The minimum length threshold
+     * @return The minimum branch length threshold
      */
-    public Vector3f getMinLength() {
+    public float getMinLength() {
         return this.minLength;
     }
 
     /**
-     * Sets the minimum length threshold of the branch generator
+     * Sets the minimum branch length threshold of the branch generator
      * <p>
      * This implementation is used by the constructor, so subclasses cannot override this method.
      *
      * @param minLength The new minimum length threshold
      * @return The minimum length threshold
      */
-    public final Vector3f setMinLength(Vector3f minLength) {
-        if (minLength.x <= 0 || minLength.y <= 0 || minLength.z <= 0) {
-            throw new IllegalArgumentException("One of the Minimum Length's Axis is lower than or equal to zero");
+    public final float setMinLength(float minLength) {
+        if (minLength <= 0) {
+            throw new IllegalArgumentException("The minimum length value is lower than or equal to zero");
         }
-        Vector3f prevLength = this.minLength;
+        float prevLength = this.minLength;
         this.minLength = minLength;
         return prevLength;
     }
 
-    /** Gets the maximum length threshold
+    /** Gets the maximum branch length threshold
      *
-     * @return The maximum length threshold
+     * @return The maximum branch length threshold
      */
-    public Vector3f getMaxLength() {
+    public float getMaxLength() {
         return this.maxLength;
     }
 
     /**
-     * Sets the maximum length threshold of the branch generator
+     * Sets the maximum branch length threshold of the branch generator
      * <p>
      * This implementation is used by the constructor, so subclasses cannot override this method.
      *
      * @param maxLength The new maximum length threshold
-     * @return The previous maximum length threshold
+     * @return The previous minimum length threshold
      */
-    public final Vector3f setMaxLength(Vector3f maxLength) {
-        if (maxLength.x <= 0 || maxLength.y <= 0 || maxLength.z <= 0) {
-            throw new IllegalArgumentException("One of the Maximum Length's Axis is lower than or equal to zero");
+    public final float setMaxLength(float maxLength) {
+        if (maxLength <= 0) {
+            throw new IllegalArgumentException("The maximum length's value is lower than or equal to zero");
         }
-        Vector3f prevLength = this.maxLength;
+        float prevLength = this.maxLength;
         this.maxLength = maxLength;
         return prevLength;
     }
 
-    /** Gets the starting endpoint
+    /** Gets the minimum angle threshold
      *
-     * @return The starting endpoint
+     * @return The minimum angle threshold
      */
-    public Vector3f getStart() {
-        return this.start;
+    public Vector3f getMinAngle() {
+        return this.minAngle;
     }
 
     /**
-     * Sets the starting point of the line.
+     * Sets the minimum angle threshold of the branch generator
      * <p>
      * This implementation is used by the constructor, so subclasses cannot override this method.
      *
-     * @param start The new starting point of the line
-     * @return The previous starting point
-    */
-    public final Vector3f setStart(Vector3f start) {
-        if (start.equals(this.end)) {
-            throw new IllegalArgumentException("Endpoints must not be equal");
-        }
-        Vector3f prevStart = this.start;
-        this.start = start;
-        return prevStart;
+     * @param minAngle The new minimum angle threshold
+     * @return The minimum angle threshold
+     */
+    public final Vector3f setMinAngle(Vector3f minAngle) {
+        Vector3f prevAngle = this.minAngle;
+        this.minAngle = minAngle;
+        return prevAngle;
     }
 
-    /** Gets the ending endpoint
+    /** Gets the maximum angle threshold
      *
-     * @return The ending endpoint
+     * @return The maximum angle threshold
      */
-    public Vector3f getEnd() {
-        return this.end;
+    public Vector3f getMaxDeviation() {
+        return this.maxAngle;
     }
 
     /**
-     * Sets the ending point of the line.
+     * Sets the maximum angle threshold of the branch generator
      * <p>
      * This implementation is used by the constructor, so subclasses cannot override this method.
      *
-     * @param end The new ending point of the line
-     * @return The previous ending point
-    */
-    public final Vector3f setEnd(Vector3f end) {
-        if (end.equals(this.start)) {
-            throw new IllegalArgumentException("Endpoints must not be equal");
-        }
-        Vector3f prevEnd = this.end;
-        this.end = end;
-        return prevEnd;
+     * @param maxAngle The new maximum angle threshold
+     * @return The previous maximum angle threshold
+     */
+    public final Vector3f setMaxAngle(Vector3f maxAngle) {
+        Vector3f prevAngle = this.maxAngle;
+        this.maxAngle = maxAngle;
+        return prevAngle;
     }
 
-    /** Gets the branch subdivisions
+    /** Gets the maximum branch count
      *
-     * @return The branch subdivisions
+     * @return The maximum branch count
      */
-    public int getSubdivisions() {
-        return this.subDivisions;
+    public int getMaxBranches() {
+        return this.maxBranchCount;
     }
 
     /**
-     * Sets the  branch subdivisions of the branch generator
+     * Sets the maximum branches for the branch generator
      * <p>
      * This implementation is used by the constructor, so subclasses cannot override this method.
      *
-     * @param subDivisions The new branch subdivision count
-     * @return The previous branch subdivision count
+     * @param branchCount The new maximum branch count
+     * @return The previous maximum branch count
      */
-    public final int setSubDivisions(int subDivisions) {
-        if (subDivisions <= 0) {
-            throw new IllegalArgumentException("Sub Divisions has to be positive and non-zero");
+    public final int setMaxBranches(int branchCount) {
+        if (branchCount <= 0) {
+            throw new IllegalArgumentException("Maximum Branch Count has to be positive and non-zero");
         }
-        int prevSubDivisions = this.subDivisions;
-        this.subDivisions = subDivisions;
-        return prevSubDivisions;
+        int prevBranchCount = this.maxBranchCount;
+        this.maxBranchCount = branchCount;
+        return prevBranchCount;
+    }
+
+    /** Gets the minimum branch count
+     *
+     * @return The minimum branch count
+     */
+    public int getMinimumBranches() {
+        return this.minBranchCount;
+    }
+
+    /**
+     * Sets the minimum branches for the branch generator
+     * <p>
+     * This implementation is used by the constructor, so subclasses cannot override this method.
+     *
+     * @param branchCount The new minimum branch count
+     * @return The previous minimum branch count
+     */
+    public final int setMinBranches(int branchCount) {
+        if (branchCount <= 0) {
+            throw new IllegalArgumentException("Minimum Branch Count has to be positive and non-zero");
+        }
+        int prevBranchCount = this.minBranchCount;
+        this.minBranchCount = branchCount;
+        return prevBranchCount;
+    }
+
+    private void generateFractal(ApelServerRenderer renderer, DrawContext drawContext, Vector3f start, int subdivs) {
+        if (subdivs == this.maxBranchCount || (subdivs >= this.minBranchCount && rand.nextBoolean())) return;
+        float currLength = MathHelper.nextFloat(rand, this.minLength, this.maxLength);
+        float currAngleX = MathHelper.nextFloat(rand, this.minAngle.x, this.maxAngle.x);
+        float currAngleY = MathHelper.nextFloat(rand, this.minAngle.y, this.maxAngle.y);
+        float currAngleZ = MathHelper.nextFloat(rand, this.minAngle.z, this.maxAngle.z);
+        Vector3f end = new Vector3f(start).add(currLength, currLength, currLength);
+        renderer.drawLine(
+                this.particleEffect,
+                drawContext.getCurrentStep(),
+                drawContext.getPosition(),
+                new Vector3f(start).add(drawContext.getPosition()),
+                end.add(drawContext.getPosition()),
+                this.rotation,
+                this.amount
+        );
+        subdivs += 1;
+        generateFractal(renderer, drawContext, end.rotateZ(currAngleZ).rotateY(currAngleY).rotateX(currAngleX), subdivs);
     }
 
 
     @Override
     public void draw(ApelServerRenderer renderer, DrawContext drawContext) {
-        Vector3f objectDrawPos = new Vector3f(drawContext.getPosition()).add(this.offset);
-
-        generateBranch(renderer, drawContext, this.start, this.end, this.subDivisions);
-    }
-
-
-    private void generateBranch(ApelServerRenderer renderer, DrawContext drawContext, Vector3f from, Vector3f to, int depth) {
-        if (depth <= 0) {
-            renderer.drawParticle(this.particleEffect, drawContext.getCurrentStep(), to);
-            return;
-        }
-
-        Vector3f dir = new Vector3f(to).sub(from);
-        float dist = dir.length();
-        dir.normalize();
-
-        float segmentLen;
-        if (from.distance(end) < 1e-3f) {
-            float angle = rand.nextFloat() * (float) Math.PI * 2;
-            dir.set((float)Math.cos(angle), (float)Math.sin(angle), 0);
-            segmentLen = minLength.length() + rand.nextFloat() * (maxLength.length() - minLength.length());
-        } else {
-            float t = Math.min(1f, from.distance(end) / maxLength.length());
-            segmentLen = minLength.length() + t * (maxLength.length() - minLength.length());
-        }
-
-        Vector3f mid = new Vector3f(from).fma(segmentLen, dir);
-
-        renderer.drawParticle(this.particleEffect, drawContext.getCurrentStep(), mid);
-
-        generateBranch(renderer, drawContext, mid, to, depth - 1);
-
-        Vector3f dev = new Vector3f(dir);
-        dev.rotateY((rand.nextFloat() - 0.5f) * 0.5f);
-        Vector3f devEnd = new Vector3f(mid).fma(segmentLen, dev);
-        generateBranch(renderer, drawContext, mid, devEnd, depth - 1);
+        generateFractal(renderer, drawContext, new Vector3f(0), 0);
     }
 
     public static class Builder<B extends Builder<B>> extends ParticleObject.Builder<B, ParticleBranchGen> {
-        protected Vector3f start = new Vector3f();
-        protected Vector3f end = new Vector3f();
-        protected Vector3f minDimensionsThreshold = new Vector3f();
-        protected Vector3f maxDimensionsThreshold = new Vector3f();
-        protected int subDivs = 1;
+        protected float minLengthThreshold;
+        protected float maxLengthThreshold;
+        protected int maxBranches = 1;
+        protected int minBranches = 1;
+        protected Vector3f minAngle;
+        protected Vector3f maxAngle;
 
         private Builder() {}
 
         /**
-         * Set the number of subdivisions on the builder. This method is not cumulative;
+         * Set the number of maximum branches on the builder. This method is not cumulative;
          * repeated calls will overwrite the value.
          */
-        public B subDivisions(int subDivisions) {
-            if (subDivisions <= 0) {
-                throw new IllegalArgumentException("The Argument subDivisions is negative or equal to zero");
+        public B maximumBranches(int maxBranches) {
+            if (maxBranches <= 0) {
+                throw new IllegalArgumentException("The Argument maxBranches is negative or equal to zero");
             }
-            this.subDivs = subDivisions;
+            this.maxBranches = maxBranches;
             return self();
         }
 
         /**
-         * Set the minimum dimensions threshold on the builder. This method is not cumulative;
+         * Set the number of minimum branches on the builder. This method is not cumulative;
          * repeated calls will overwrite the value.
          */
-        public B minDimensions(Vector3f minimumDimensionsThreshold) {
-            this.minDimensionsThreshold = minimumDimensionsThreshold;
+        public B minimumBranches(int minBranches) {
+            if (minBranches <= 0) {
+                throw new IllegalArgumentException("The Argument minBranches is negative or equal to zero");
+            }
+            this.maxBranches = minBranches;
             return self();
         }
 
         /**
-         * Set the maximum dimensions threshold on the builder. This method is not cumulative;
+         * Set the minimum branch angle on the builder. This method is not cumulative;
          * repeated calls will overwrite the value.
          */
-        public B maxDimensions(Vector3f maximumDimensionsThreshold) {
-            this.maxDimensionsThreshold = maximumDimensionsThreshold;
+        public B minimumAngle(Vector3f minAngle) {
+            this.minAngle = minAngle;
             return self();
         }
 
         /**
-         * Set the start point on the builder.  This method is not cumulative; repeated calls will overwrite the value.
+         * Set the maximum branch angle on the builder. This method is not cumulative;
+         * repeated calls will overwrite the value.
          */
-        public B start(Vector3f start) {
-            this.start = start;
+        public B maximumAngle(Vector3f maxAngle) {
+            this.maxAngle = maxAngle;
             return self();
         }
 
         /**
-         * Set the end point on the builder.  This method is not cumulative; repeated calls will overwrite the value.
+         * Set the minimum branch length the builder. This method is not cumulative;
+         * repeated calls will overwrite the value.
          */
-        public B end(Vector3f end) {
-            this.end = end;
+        public B minLengthThreshold(float minLength) {
+            this.minLengthThreshold = minLength;
+            return self();
+        }
+
+        /**
+         * Set the maximum branch length on the builder. This method is not cumulative;
+         * repeated calls will overwrite the value.
+         */
+        public B maxLengthThreshold(float maxLength) {
+            this.maxLengthThreshold = maxLength;
             return self();
         }
 
