78ed1f3a345f828245acfff4127430e68d39f589
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-05-08 19:02:32 +0300

    Rewrote the documentation according to the recent change and adjusted the code for that change

--- src/main/java/net/mcbrincie/apel/lib/easing/StatefulEasingCurve.java
+++ src/main/java/net/mcbrincie/apel/lib/easing/StatefulEasingCurve.java
@@ -4,10 +4,10 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
 import org.joml.*;
 
-/** This is the base class for StatelessEasingCurve. It provides a lot of overloaded methods for interpolating
- * between a starting value and an ending value. Classes can extend from this StatelessEasingCurve and won't
- * have to deal with the other overloaded methods. They simply provide the math logic for the floating number overload,
- * and this base class takes care of translating it into the other data types. Supported datatypes are:
+/** This is the base class for {@link StatefulEasingCurve}. It provides a lot of methods for interpolating
+ * between a starting value and an ending value. Classes can extend from this {@link StatefulEasingCurve} and won't
+ * have to deal with the other methods. They simply provide the math logic for the {@code interpolate(float t)}
+ * method, and this base class takes care of translating it into the other data types. Supported datatypes are:
  * <p>
  * - {@link Integer}<br>
  * - {@link Double}<br>
@@ -24,22 +24,26 @@ import org.joml.*;
  * - {@link Vector2d}<br>
  * - {@link Vector2L}<br>
  * <p>
- * The {@link StatefulEasingCurve} class has multiple compute methods such as {@code compute(float t)},
- * {@code computeVec2(Vector2f t)}, {@code computeVec3(float t)}... and so on. These compute methods are
- * meant to be used by outside developers. Depending on the vector's size or if it is just a number, there will
- * be available certain compute methods to use. Each compute method computes the dimension requested and returns
- * a value that has the same dimensions as the compute's method name suggests
+ * The {@link StatefulEasingCurve} class has 2 primary methods for computing values using the easing curve. The first is by
+ * using {@code compute(t)} which takes a t parameter and spits out a newly computed t parameter based on the implementation
+ * of the easing curve. The Compute methods are a unified way of handling scalar and vector values; ideally they shouldn't be
+ * overridden. The compute methods do not take into account the start and end parameters. The other method is {@code getValue(t)},
+ * unlike compute method it does take into account the start and end parameters. It first calculates the t value using the
+ * aforementioned compute methods and then linearly interpolates(lerp) between the starting and ending values to create
+ * a new intermediate value to then return
  * </p>
  * <p>
- * You can supply different t values for each dimension or one t value for all dimensions when using a
- * compute method. All the Compute methods call internally {@code interporlate(float x, float y, float t)} which
- * is where the implementation of the easing curve lies in. <b>If you plan on inheriting from this StatelessEasingCurve. Be
- * sure to override ONLY the interpolate method</b>
+ * When using either of the 2 methods, you can also supply a vector value as t, which returns a new vector with the
+ * computed results. You can have up to 3D vectors (because of the types supported). <u>Do note however that if you decide
+ * to supply a {@link Vector2f} and the start & end happen to not have the same dimensions. It will error out</u>
  * </p>
  * <p>
- * Lastly, providing lower dimensions than what the compute method used for is built, will result in a
- * {@code RuntimeException} so be sure to know what vector type you are using and calling the appropriate
- * compute method
+ * Lastly, providing any higher dimensions of t, when the start and end values have a lower dimension, will only use
+ * the corresponding values to compute and not the higher dimensional values. This type of easing curve also has an EaseType
+ * to control the shape of the easing curve which can be either<br>
+ * - <b>Ease In</b> (Grows Slowly -> Then it ramps up the speed)<br>
+ * - <b>Ease Out</b> (Grows Fast -> Then it slows down)<br>
+ * - <b>Ease In Out</b> (Grows Slowly -> then it ramps up a bit of speed -> then slows down again)
  * </p>
  *
  * @param <T> The type to use for the computation (can be either a vector or a scalar value)
@@ -49,20 +53,22 @@ public abstract class StatefulEasingCurve<T> extends EasingCurve<T> {
     protected EaseType easeType;
 
 
-    public StatefulEasingCurve(T start, T end, EaseType easeType) {
-        super(start, end);
+    public StatefulEasingCurve(T start, T end, EaseType easeType, float easingProgressFactor) {
+        super(start, end, easingProgressFactor);
         this.easeType = easeType;
+    }
+
+    public StatefulEasingCurve(T start, T end, EaseType easeType) {
+        this(start, end, easeType, 0);
     }
 
 
     /** This is the interpolate method. It is the implementation of all the easing logic,
-     * it receives a start parameter, an end parameter and a t value. It then returns the computed value
-     * based on the provided parameters
+     * a t value. It then returns the computed intermediate value based on the start and end
+     * as well as the provided t parameter
      *
-     * @param a The starting value
-     * @param b The ending value
      * @param t The time value(t)
      * @return The computed value out of all the 3 parameters from the implementation
      */
-    protected abstract float interpolate(float a, float b, float t);
+    protected abstract float interpolate(float t);
 }
