a9fa103c9f582bf7aace86eafddbb6d3afe95310
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-05-08 13:48:15 +0300

    Test and testing

--- src/main/java/net/mcbrincie/apel/item/DebugParticleWand2.java
+++ src/main/java/net/mcbrincie/apel/item/DebugParticleWand2.java
@@ -1,7 +1,9 @@
 package net.mcbrincie.apel.item;
 
 import net.mcbrincie.apel.lib.animators.PointAnimator;
+import net.mcbrincie.apel.lib.easing.LinearEasingCurve;
 import net.mcbrincie.apel.lib.objects.ParticleBranchGen;
+import net.mcbrincie.apel.lib.objects.ParticleSphere;
 import net.mcbrincie.apel.lib.renderers.ApelServerRenderer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -21,21 +23,18 @@ public class DebugParticleWand2 extends Item {
     public ActionResult use(World world, PlayerEntity user, Hand hand) {
         if (world.isClient) return ActionResult.PASS;
 
-        ParticleBranchGen particleBranchGen = ParticleBranchGen.builder()
-                .amount(20)
-                .particleEffect(ParticleTypes.ELECTRIC_SPARK)
-                .minimumAngle(new Vector3f(0))
-                .maximumAngle(new Vector3f((float) Math.PI / 12))
-                .constantLengthThreshold(2f)
-                .minimumBranchesPerDivision(4)
-                .maximumBranchesPerDivision(12)
-                .constantTotalBranches(4)
+        LinearEasingCurve<Float> linearEasingCurve = new LinearEasingCurve<>(3f, 8f);
+        linearEasingCurve.setEaseProgressFactor(1.2f);
+        ParticleSphere particleSphere = ParticleSphere.builder()
+                .radius(linearEasingCurve)
+                .amount(100)
+                .particleEffect(ParticleTypes.END_ROD)
                 .build();
 
         PointAnimator pointAnimator = PointAnimator.builder()
                 .point(new Vector3f(0))
-                .particleObject(particleBranchGen)
-                .renderingSteps(200)
+                .particleObject(particleSphere)
+                .renderingSteps(300)
                 .build();
 
         ApelServerRenderer apelServerRenderer = ApelServerRenderer.client((ServerWorld) world);
