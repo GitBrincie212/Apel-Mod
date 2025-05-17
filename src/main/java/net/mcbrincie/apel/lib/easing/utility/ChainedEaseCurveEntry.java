27388aa12c0ef885fa6ac59e61012972c23813c4
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-05-17 11:57:24 +0300

    Removing the ChainedEaseCurve and will be adding it on a later update due to its complexity

--- src/main/java/net/mcbrincie/apel/lib/easing/utility/ChainedEaseCurveEntry.java
+++
@@ -1,6 +0,0 @@
-package net.mcbrincie.apel.lib.easing.utility;
-
-import net.mcbrincie.apel.lib.easing.EasingCurve;
-
-public record ChainedEaseCurveEntry<T>(EasingCurve<T> easingCurve, float end) {
-}
