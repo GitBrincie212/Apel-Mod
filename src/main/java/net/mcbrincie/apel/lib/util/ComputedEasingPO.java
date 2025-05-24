af7936ff0870c115c0141857d477e92092f7e989
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-05-24 20:34:16 +0300

    Added a ComputedEasingRPO and moved the amount calculations on it

--- src/main/java/net/mcbrincie/apel/lib/util/ComputedEasingPO.java
+++ src/main/java/net/mcbrincie/apel/lib/util/ComputedEasingPO.java
@@ -6,15 +6,10 @@ import org.joml.Vector3f;
 public class ComputedEasingPO extends ComputedEasings {
     public final Vector3f computedRotation;
     public final Vector3f computedOffset;
-    public final int computedAmount;
 
     public ComputedEasingPO(ParticleObject<?> particleObject, float currStep, float numberOfSteps) {
         super(currStep, numberOfSteps);
-        this.computedAmount = particleObject.getAmount().getValue(this.tVal);
         this.computedRotation = ApelUtils.normalizeRotation(particleObject.getRotation().getValue(this.tVal));
         this.computedOffset = particleObject.getOffset().getValue(this.tVal);
-        if (this.computedAmount <= 0) {
-            throw new IllegalArgumentException("Amount of particles has to be above 0");
-        }
     }
 }
