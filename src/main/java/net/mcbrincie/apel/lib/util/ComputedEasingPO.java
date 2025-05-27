d65988737056b6c0f4dfcc814ae7bd5510ee806b
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-05-27 10:37:35 +0300

    Added scale as a parameter to compute

--- src/main/java/net/mcbrincie/apel/lib/util/ComputedEasingPO.java
+++ src/main/java/net/mcbrincie/apel/lib/util/ComputedEasingPO.java
@@ -6,10 +6,12 @@ import org.joml.Vector3f;
 public class ComputedEasingPO extends ComputedEasings<ComputedEasingPO> {
     public final Vector3f computedRotation;
     public final Vector3f computedOffset;
+    public final Vector3f computedScale;
 
     public ComputedEasingPO(ParticleObject<?> particleObject, float currStep, float numberOfSteps) {
         super(currStep, numberOfSteps);
         this.computedRotation = ApelUtils.normalizeRotation(particleObject.getRotation().getValue(this.tVal));
+        this.computedScale = particleObject.getScale().getValue(this.tVal);
         this.computedOffset = particleObject.getOffset().getValue(this.tVal);
     }
 }
