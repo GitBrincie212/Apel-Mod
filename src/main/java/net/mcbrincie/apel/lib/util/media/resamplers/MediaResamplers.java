ad1c031f0a991e9a72520a4e9ae5eba744277c0f
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-08-06 12:35:17 +0300

    Added documentation to the MediaResamplers

--- src/main/java/net/mcbrincie/apel/lib/util/media/resamplers/MediaResamplers.java
+++ src/main/java/net/mcbrincie/apel/lib/util/media/resamplers/MediaResamplers.java
@@ -2,14 +2,42 @@ package net.mcbrincie.apel.lib.util.media.resamplers;
 
 import net.mcbrincie.apel.lib.easing.EasingCurve;
 import net.mcbrincie.apel.lib.easing.shaped.ConstantEasingCurve;
+import net.mcbrincie.apel.lib.objects.ParticleMedia;
 import net.minecraft.util.math.ColorHelper;
 
+/** {@link MediaResamplers} offers built-in some commonly used media resamplers such as:
+ * <ul>
+ *     <li>Nearest Neighbor Resampling</li>
+ *     <li>Bilinear Resampling</li>
+ * </ul>
+ * All of the resamplers support the use of EasingCurves, except for {@link #retain()} which is a special resampler
+ * designed to return the same frame data (effectively skipping the resampling part)
+ *
+ * @see ParticleMedia
+ * @see MediaResampler
+ */
 @SuppressWarnings("unused")
 public class MediaResamplers {
+    /** Constructs the retain media resampler, returning the function to be used. It acts more as an identity function
+     * where it gives the same output back as the input. It is useful when you want to keep the original dimensions of the
+     * media (effectively skipping the media resampling process in the pipeline)
+     *
+     * @return The constructed retain media resampler function
+     */
     public static MediaResampler retain() {
         return (frame, t) -> {};
     }
 
+    /** Constructs the nearest neighbor media resampler, returning the function to be used. It is one of the fastest
+     * media resamplers out there, but one of the less pretty looking ones. It is generally the most commonly used media
+     * resampler due to its performance while still giving good results
+     * <br /> <br />
+     * This overload requires you to give it the dimensions as easing curves. For a constant one, check out {@link #nearestNeighbour(int, int)}
+     *
+     * @param destWidth The target width to resize to
+     * @param destHeight The target height to resize to
+     * @return The constructed nearest neighbor media resampler function
+    */
     public static MediaResampler nearestNeighbour(EasingCurve<Integer> destWidth, EasingCurve<Integer> destHeight) {
         return (data, t) -> {
             int destW = destWidth.getValue(t);
@@ -31,10 +59,29 @@ public class MediaResamplers {
         };
     }
 
+    /** Constructs the nearest neighbor media resampler, returning the function to be used. It is one of the fastest
+     * media resamplers out there, but one of the less pretty looking ones. It is generally the most commonly used media
+     * resampler due to its performance while still giving good results
+     * <br /> <br />
+     * This overload requires you to give it the dimensions as constants. For an easing curve one, check out {@link #nearestNeighbour(EasingCurve, EasingCurve)}
+     *
+     * @param destWidth The target width to resize to
+     * @param destHeight The target height to resize to
+     * @return The constructed nearest neighbor media resampler function
+     */
     public static MediaResampler nearestNeighbour(int destWidth, int destHeight) {
         return nearestNeighbour(new ConstantEasingCurve<>(destWidth), new ConstantEasingCurve<>(destHeight));
     }
 
+    /** Constructs the bilinear media resampler, returning the function to be used. It is still fast but gives more
+     * smooth results, it is commonly used on large medias where quality matters more than performance
+     * <br /> <br />
+     * This overload requires you to give it the dimensions as easing curves. For a constant one, check out {@link #bilinear(int, int)}
+     *
+     * @param destWidth The target width to resize to
+     * @param destHeight The target height to resize to
+     * @return The constructed bilinear media resampler function
+     */
     public static MediaResampler bilinear(EasingCurve<Integer> destWidth, EasingCurve<Integer> destHeight) {
         return (data, t) -> {
             int destW = destWidth.getValue(t);
@@ -72,6 +119,16 @@ public class MediaResamplers {
         };
     }
 
+
+    /** Constructs the bilinear media resampler, returning the function to be used. It is still fast but gives more
+     * smooth results, it is commonly used on large medias where quality matters more than performance
+     * <br /> <br />
+     * This overload requires you to give it the dimensions as constants. For an easing curve one, check out {@link #bilinear(EasingCurve, EasingCurve)}
+     *
+     * @param destWidth The target width to resize to
+     * @param destHeight The target height to resize to
+     * @return The constructed bilinear media resampler function
+    */
     public static MediaResampler bilinear(int destWidth, int destHeight) {
         return bilinear(new ConstantEasingCurve<>(destWidth), new ConstantEasingCurve<>(destHeight));
     }
