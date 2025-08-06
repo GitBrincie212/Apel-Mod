c0570ff1d82ffdec1ae58cc8d8b909865f3f820c
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-08-06 13:05:57 +0300

    Added documentation for MediaFilter

--- src/main/java/net/mcbrincie/apel/lib/util/media/filters/MediaFilter.java
+++ src/main/java/net/mcbrincie/apel/lib/util/media/filters/MediaFilter.java
@@ -2,6 +2,13 @@ package net.mcbrincie.apel.lib.util.media.filters;
 
 import net.mcbrincie.apel.lib.objects.ParticleMedia;
 
+/** This is the media filter. It takes the frame data and a t parameter (for the computing of the easing curves) and
+ * modifies this frame data depending on the filter itself, returning nothing in the process. There are builtin filters
+ * to choose from in {@link MediaFilters}
+ *
+ * @see MediaFilters
+ * @see ParticleMedia
+ */
 @FunctionalInterface
 public interface MediaFilter {
     void apply(ParticleMedia.FrameData data, float t);
