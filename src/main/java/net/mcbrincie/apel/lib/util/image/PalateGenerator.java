95fe0a5823a52379eecac81ec969350b10fd9b0d
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-08-06 11:49:26 +0300

    Removed the PalateGenerators in favor of the new ParticleMedia

--- src/main/java/net/mcbrincie/apel/lib/util/image/PalateGenerator.java
+++
@@ -1,9 +0,0 @@
-package net.mcbrincie.apel.lib.util.image;
-
-import net.minecraft.particle.ParticleEffect;
-import org.joml.Vector3f;
-
-@FunctionalInterface
-public interface PalateGenerator {
-    ParticleEffect apply(int rgba, int x, int y, Vector3f position);
-}
